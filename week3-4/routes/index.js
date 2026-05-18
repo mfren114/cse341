7f6e106b7e88081008e5a4d15accb1504b2a054e
mfren114 <mathew.frenette@yahoo.com>
2026-05-18 15:00:20 -0700

    Swagger

--- week3-4/routes/index.js
+++ week3-4/routes/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 
+router.use('/', require('./swagger'));
+
 router.get('/', (req, res) => {
+    //#swagger.tags=['Personal Project Test']
     res.send('Personal Project Test');
 });
 
